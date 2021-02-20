db4347c7150fe0f166017e24ea47500987c4399b
TheLDB <ldb@erikboles.com>
2021-02-20 16:31:05 -0700

    ok it works, ill work on it sdome more later

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('Pong!');
+    }
+  });
+
 client.login(process.env.token);
